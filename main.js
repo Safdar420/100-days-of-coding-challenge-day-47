6ebbfe05d8c4a5d29d8a86c0d8acb16229c48ebf
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-05-01 23:45:20 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+//  Q n0 139
+var count1 = 5;
+if (count1 > 0) {
+    console.log("count is greater than 0");
+}
+function add1(a, b) {
+    return a + b;
+}
+//  Q no 140
+console.log("Using a reserved word as a variable name causes a syntax error in JavaScript.");
